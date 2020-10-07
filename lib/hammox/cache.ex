efd5b33e70603ccfcfe591ee93447332bf380fd1
Michał Szewczak <michal@duffel.com>
2020-10-07 20:43:20 +0100

    add Hammox.Cache

---
+++ lib/hammox/cache.ex
@@ -0,0 +1,15 @@
+defmodule Hammox.Cache do
+  use Agent
+
+  def start_link(_initial_value) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def put(key, value) do
+    Agent.update(__MODULE__, &Map.put(&1, key, value))
+  end
+
+  def get(key) do
+    Agent.get(__MODULE__, &Map.get(&1, key))
+  end
+end
