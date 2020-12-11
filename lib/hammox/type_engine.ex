40225d21d65baa2adb5a8fd42b8f9fa992e502c7
Randy Coulman <rcoulman@influxdata.com>
2020-12-11 11:27:57 -0800

    Address PR feedback
    Simplify the handling of the recursive matches on struct-like maps.

--- lib/hammox/type_engine.ex
+++ lib/hammox/type_engine.ex
@@ -354,19 +354,10 @@ defmodule Hammox.TypeEngine do
         {:error, [{:struct_name_type_mismatch, struct_name, other_struct_name}]}
 
       [{:type, _, :map_field_exact, [{:atom, _, :__struct__}, {:type, _, _, _} = struct_type]}] ->
-        case {match_type(struct_name, struct_type),
-              match_type(Map.delete(value, :__struct__), {:type, 0, :map, rest_field_types})} do
-          {:ok, :ok} ->
-            :ok
-
-          {:ok, error} ->
-            error
-
-          {error, :ok} ->
-            error
-
-          {{:error, struct_reasons}, {:error, rest_reasons}} ->
-            {:error, struct_reasons ++ rest_reasons}
+        with :ok <- match_type(struct_name, struct_type),
+             :ok <-
+               match_type(Map.delete(value, :__struct__), {:type, 0, :map, rest_field_types}) do
+          :ok
         end
     end
   end
