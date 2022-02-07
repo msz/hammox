93e0f464b6485b23f3a55ea9942c4798f5d196a4
Sean Lewis <sean@genesisblock.com>
2022-02-07 13:35:31 -0700

    add telemetry back in for cache

--- lib/hammox/cache.ex
+++ lib/hammox/cache.ex
@@ -7,49 +7,43 @@ defmodule Hammox.Cache do
   end
 
   def init(_blah) do
-    :ets.new(:typespec_cache, [:named_table, :public])
+    :ets.new(:typespec_cache, [:named_table])
     {:ok, %{}}
   end
 
   def put(key, value) do
-    #:telemetry.span(
-      #[:hammox, :cache_put],
-      #%{key: key, value: value},
-      #fn ->
-        #result = GenServer.call(__MODULE__, {:put, key, value})
-        #{result, %{}}
-      #end
-    #)
-    #GenServer.call(__MODULE__, {:put, key, value})
-    :ets.insert(:typespec_cache, {key, value})
-    :ok
+    :telemetry.span(
+      [:hammox, :cache_put],
+      %{key: key, value: value},
+      fn ->
+        result = GenServer.call(__MODULE__, {:put, key, value})
+        {result, %{}}
+      end
+    )
   end
 
   def get(key) do
-    #:telemetry.span(
-      #[:hammox, :cache_get],
-      #%{key: key},
-      #fn ->
-        #result = GenServer.call(__MODULE__, {:get, key})
-        #{result, %{}}
-      #end
-    #)
-    #GenServer.call(__MODULE__, {:get, key})
-    list = :ets.lookup(:typespec_cache, key)
-    if length(list) > 0 do
-      {_key, value} = hd(list)
-      value
-    else
-      nil
-    end
+    :telemetry.span(
+      [:hammox, :cache_get],
+      %{key: key},
+      fn ->
+        result = :ets.lookup(:typespec_cache, key)
+                 |> process_lookup()
+        {result, %{}}
+      end
+    )
   end
 
-  #def handle_call({:get, key}, _from, storage) do
-    #{:reply, Map.get(storage, key), storage}
-  #end
+  defp process_lookup([{_key, value}] = _lookup_result) do
+    value
+  end
 
-  #def handle_call({:put, key, value}, _from, _storage) do
-    #:ets.insert(:typespec_cache, {key, value})
-    #{:reply, :ok, %{}}
-  #end
+  defp process_lookup(_result) do
+    nil
+  end
+
+  def handle_call({:put, key, value}, _from, _storage) do
+    :ets.insert(:typespec_cache, {key, value})
+    {:reply, :ok, %{}}
+  end
 end
