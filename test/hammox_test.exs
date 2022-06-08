8fcd5d456860c46407336ed2ee7488bfbea398a2
Sean Lewis <sean@hightop.com>
2022-06-08 13:49:11 -0600

    remove test change

--- test/hammox_test.exs
+++ test/hammox_test.exs
@@ -1320,8 +1320,7 @@ defmodule HammoxTest do
 
   defp assert_pass(function_name, value) do
     TestMock |> expect(function_name, fn -> value end)
-    result = apply(TestMock, function_name, [])
-    assert value == result
+    assert value == apply(TestMock, function_name, [])
   end
 
   defp assert_fail(function_name, value) do
