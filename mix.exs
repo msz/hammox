365dcd3dcc30ab98f83177895683bea0ad0b26f3
Michał Szewczak <michal@szewczak@email>
2025-07-26 23:42:03 +0100

    v0.7.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Hammox.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/msz/hammox"
-  @version "0.7.0"
+  @version "0.7.1"
 
   def project do
     [
@@ -64,7 +64,6 @@ defmodule Hammox.MixProject do
         "README.md": [title: "Overview"],
         "guides/Telemetry.md": [title: "Telemetry"],
         LICENSE: [title: "License"]
-
       ],
       main: "readme",
       source_url: @source_url,
