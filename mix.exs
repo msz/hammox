b57ed9e417e7d2df0aab6714c2ca32ff126de002
Michał Szewczak <michal@szewczak.email>
2026-07-07 15:09:21 +0100

    require elixir 1.15

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hammox.MixProject do
     [
       app: :hammox,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
